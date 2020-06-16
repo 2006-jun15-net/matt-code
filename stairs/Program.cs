87567b617c20316a83655d9090b5c20da31cc6d3
Fortiek <fortiek@gmail.com>
2020-06-16 17:46:21 -0400

    The 8 stairs project.

---
+++ stairs/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace stairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //run a for loop over an array of strings
+         string[] stairs = {"       #", "      ##", "     ###", "    ####", "   #####", "  ######", " #######", "########"};
+
+         for(int i=0; i<stairs.Length;i++)
+         {
+             Console.WriteLine(stairs[i]);
+         }
+        }
+    }
+}
