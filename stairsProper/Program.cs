d32cf419e3902aa42bb278dbfce5f7bd6ae2732c
Fortiek <fortiek@gmail.com>
2020-06-16 20:41:32 -0400

    Doing the ascending stairs problem the right way.... allow input to make stairs ascending in proper order.

---
+++ stairsProper/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace stairsProper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Strings are immutable; need to find a way to repeat input of char/line
+            Console.WriteLine("Enter the number of steps to add.\n");
+
+            int numOfSteps = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= numOfSteps; i++)
+            {
+                for (int j = 1; j <=numOfSteps-i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                { 
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
